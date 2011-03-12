50368ac7a869416b2d69c51cd0a2c508ac6d3f8f
Vasyl Dizhak <dijakroot@gmail.com>
2011-03-12 22:57:31 +0200

    Simple testing

--- src/twillio_app/apps/views.py
+++ src/twillio_app/apps/views.py
@@ -3,7 +3,15 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 
+import pdb
+
 
 def twillio(request):
+    if request.method == 'POST':
+        return render_to_response('base.xml', {},
+            mimetype="application/xml")
+        print request
     return render_to_response('base.xml', {},
         mimetype="application/xml")
+
+    
